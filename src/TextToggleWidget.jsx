264a865bf5e46424954048040f4087c5f557e79b
Machakhleli <ushangikakhidze@gmail.com>
2025-08-15 19:19:45 +0400

    feat: started building texttogglewidget file

--- src/TextToggleWidget.jsx
+++ src/TextToggleWidget.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 
 function TextToggleWidget() {
+  const [text, setText] = useState("");
+  const [isVisable, setIsVisable] = useState(true);
   return (
     <div className="p-6 bg-white rounded-lg shadow-md">
       <h3 className="text-xl font-semibold mb-4">
         ინტერაქტიული ტექსტი და გადამრთველი
       </h3>
-      {/* Input field, displayed text, toggle button, character counter */}
+      {
+        <input
+          type="text"
+          value={text}
+          onChange={(element) => setText(element.target.value)}
+          placeholder="enter text here"
+          className="max-w-md w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+        /> /* Input field, displayed text, toggle button, character counter */
+      }
     </div>
   );
 }
